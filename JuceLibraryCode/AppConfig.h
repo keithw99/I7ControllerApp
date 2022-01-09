969d138ce9756d58a5910f36762f425937644269
Keith <keithw99@gmail.com>
2022-01-09 12:13:08 -0500

    Updated to JUCE v6.1.3.

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -23,12 +23,12 @@
 /*
   ==============================================================================
 
-   In accordance with the terms of the JUCE 5 End-Use License Agreement, the
+   In accordance with the terms of the JUCE 6 End-Use License Agreement, the
    JUCE Code in SECTION A cannot be removed, changed or otherwise rendered
    ineffective unless you have a JUCE Indie or Pro license, or are using JUCE
    under the GPL v3 license.
 
-   End User License Agreement: www.juce.com/juce-5-licence
+   End User License Agreement: www.juce.com/juce-6-licence
 
   ==============================================================================
 */
@@ -39,15 +39,11 @@
  #define JUCE_DISPLAY_SPLASH_SCREEN 1
 #endif
 
-#ifndef JUCE_REPORT_APP_USAGE
- #define JUCE_REPORT_APP_USAGE 1
-#endif
-
 // END SECTION A
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x50407
+#define JUCE_PROJUCER_VERSION 0x60103
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_i7                         1
@@ -82,10 +78,6 @@
  //#define JUCE_WASAPI 1
 #endif
 
-#ifndef    JUCE_WASAPI_EXCLUSIVE
- //#define JUCE_WASAPI_EXCLUSIVE 0
-#endif
-
 #ifndef    JUCE_DIRECTSOUND
  //#define JUCE_DIRECTSOUND 1
 #endif
@@ -103,7 +95,11 @@
 #endif
 
 #ifndef    JUCE_USE_ANDROID_OBOE
- //#define JUCE_USE_ANDROID_OBOE 0
+ //#define JUCE_USE_ANDROID_OBOE 1
+#endif
+
+#ifndef    JUCE_USE_OBOE_STABILIZED_CALLBACK
+ //#define JUCE_USE_OBOE_STABILIZED_CALLBACK 0
 #endif
 
 #ifndef    JUCE_USE_ANDROID_OPENSLES
@@ -156,6 +152,10 @@
  //#define JUCE_PLUGINHOST_LADSPA 0
 #endif
 
+#ifndef    JUCE_CUSTOM_VST3_SDK
+ //#define JUCE_CUSTOM_VST3_SDK 0
+#endif
+
 //==============================================================================
 // juce_core flags:
 
@@ -197,6 +197,10 @@
 
 #ifndef    JUCE_STRICT_REFCOUNTEDPOINTER
  #define   JUCE_STRICT_REFCOUNTEDPOINTER 1
+#endif
+
+#ifndef    JUCE_ENABLE_ALLOCATION_HOOKS
+ //#define JUCE_ENABLE_ALLOCATION_HOOKS 0
 #endif
 
 //==============================================================================
@@ -259,8 +263,31 @@
  //#define JUCE_WEB_BROWSER 1
 #endif
 
+#ifndef    JUCE_USE_WIN_WEBVIEW2
+ //#define JUCE_USE_WIN_WEBVIEW2 0
+#endif
+
 #ifndef    JUCE_ENABLE_LIVE_CONSTANT_EDITOR
  //#define JUCE_ENABLE_LIVE_CONSTANT_EDITOR 0
+#endif
+
+//==============================================================================
+// juce_osc flags:
+
+#ifndef    JUCE_ALLOW_SPECIAL_CHARS_IN_ADDRESS
+ //#define JUCE_ALLOW_SPECIAL_CHARS_IN_ADDRESS 0
+#endif
+
+#ifndef    JUCE_ENABLE_BROADCAST_BY_DEFAULT
+ //#define JUCE_ENABLE_BROADCAST_BY_DEFAULT 0
+#endif
+
+#ifndef    JUCE_EXCLUSIVE_BINDING_BY_DEFAULT
+ //#define JUCE_EXCLUSIVE_BINDING_BY_DEFAULT 0
+#endif
+
+#ifndef    JUCE_IP_AND_PORT_DETECTION
+ //#define JUCE_IP_AND_PORT_DETECTION 0
 #endif
 
 //==============================================================================
